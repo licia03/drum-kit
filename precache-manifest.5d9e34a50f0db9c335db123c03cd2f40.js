dfa11d242230456c930efd040f648f651336902c
Licia Gavina <lgavina7@gmail.com>
2019-11-14 01:50:39 +0100

    Updates

---
+++ precache-manifest.5d9e34a50f0db9c335db123c03cd2f40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05d192dee32c80e4e59d83ae1bf6ba2d",
+    "url": "/drum-kit/index.html"
+  },
+  {
+    "revision": "f0d71b7896f66c9c1861",
+    "url": "/drum-kit/static/css/main.0c431bc9.chunk.css"
+  },
+  {
+    "revision": "e9c615f7003b9ef282d7",
+    "url": "/drum-kit/static/js/2.7adc1c5f.chunk.js"
+  },
+  {
+    "revision": "f0d71b7896f66c9c1861",
+    "url": "/drum-kit/static/js/main.87bd3772.chunk.js"
+  },
+  {
+    "revision": "64d29d60e505ac06a4a8",
+    "url": "/drum-kit/static/js/runtime-main.02f7b05e.js"
+  }
+]);
